fdf49f6ab90ebbd529a053dd124fedec032be9a1
Luana Savian <luana.savian@zup.com.br>
2021-10-05 18:11:28 -0300

    Criação client

---
+++ src/test/java/br/com/zup/orquestrador/OrquestradorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.orquestrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrquestradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
